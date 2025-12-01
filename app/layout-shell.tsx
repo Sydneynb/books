ae58e34f5488e13abc92d4919732b8f39fae66bd
Sydney <sydneynberthelot@gmail.com>
2025-12-01 10:39:13 -0500

    feat: dashboard layout

---
+++ app/layout-shell.tsx
@@ -0,0 +1,16 @@
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { DashboardSidebar } from '@/components/app-sidebar';
+import { ReactNode } from 'react';
+
+export default function LayoutShell({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider>
+      <div className='relative flex h-screen w-full'>
+        <DashboardSidebar />
+        <SidebarInset className='flex flex-col grow w-full'>
+          {children}
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
+  );
+}
